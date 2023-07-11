ea35d4ac1317d499ff552cf7a34379177389718f
= <=>
2023-07-11 00:04:46 -0400

    Changed Certificate path abs to relatives

--- Backend/node-server/index.js
+++ Backend/node-server/index.js
@@ -1,40 +1,43 @@
-const express = require('express');
-const https = require('https');
-const WebSocket = require('ws');
-const fs = require('fs');
-const cors = require('cors');
+const express = require("express");
+const https = require("https");
+const WebSocket = require("ws");
+const fs = require("fs");
+const cors = require("cors");
 
 const app = express();
 app.use(cors()); // Enable CORS middleware
 
-const server = https.createServer({
-    cert: fs.readFileSync('/Users/darshandave/MAC/sem3/Internship/video_streaming_platform/SSL_Certificates/liveStream_SSL/private.crt'), // Replace with your SSL certificate path
-    key: fs.readFileSync('/Users/darshandave/MAC/sem3/Internship/video_streaming_platform/SSL_Certificates/liveStream_SSL/private.pem'), // Replace with your private key path
+const server = https.createServer(
+  {
+    cert: fs.readFileSync("../../SSL_Certificates/liveStream_SSL/private.crt"), // Replace with your SSL certificate path
+    key: fs.readFileSync("../../SSL_Certificates/liveStream_SSL/private.key"), // Replace with your private key path
     passphrase: "dexter",
-}, app);
+  },
+  app
+);
 const wss = new WebSocket.Server({ server });
 
 // Store active connections
 const connections = new Set();
 
 // Handle incoming WebSocket connections
-wss.on('connection', (ws) => {
+wss.on("connection", (ws) => {
   // Add new connection to the set
   connections.add(ws);
 
   // Remove closed connections from the set
-  ws.on('close', () => {
+  ws.on("close", () => {
     connections.delete(ws);
   });
 });
 
 // Route to receive camera feed from the producer
-app.post('/feed', (req, res) => {
+app.post("/feed", (req, res) => {
   // Handle the camera feed data sent by the producer
   // and broadcast it to all connected consumers
 
   // Assuming the producer sends data as JSON
-  req.on('data', (data) => {
+  req.on("data", (data) => {
     const feedData = JSON.parse(data);
 
     // Broadcast the feed data to all connected consumers
@@ -51,5 +54,5 @@ app.post('/feed', (req, res) => {
 
 // Start the server
 server.listen(3000, () => {
-  console.log('Server listening on port 3000');
+  console.log("Server listening on port 3000");
 });
