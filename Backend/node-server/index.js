50a83ad7971673c52215997c1d59b6d7dec5d4f4
Krishna Patel <krishnapatel7046@gmail.com>
2023-07-12 13:35:44 -0400

    docker compose

--- Backend/node-server/index.js
+++ Backend/node-server/index.js
@@ -9,58 +9,58 @@ const app = express();
 // Enable CORS for all routes
 app.use(cors());
 
-const server = https.createServer(
-  {
-    cert: fs.readFileSync("../../SSL_Certificates/liveStream_SSL/private.crt"), // Replace with your SSL certificate path
-    key: fs.readFileSync("../../SSL_Certificates/liveStream_SSL/private.key"), // Replace with your private key path
-    passphrase: "dexter",
-  },
-  app
-);
+// const server = https.createServer(
+//   {
+//     cert: fs.readFileSync(), // Replace with your SSL certificate path
+//     key: fs.readFileSync(), // Replace with your private key path
+//     // passphrase: "dexter",
+//   },
+//   app
+// );
 
-const wss = new WebSocket.Server({ server });
+// const wss = new WebSocket.Server({ server });
 
-// Store active connections
-const connections = new Set();
+// // Store active connections
+// const connections = new Set();
 
-// Handle incoming WebSocket connections
-wss.on("connection", (ws) => {
-  // Add new connection to the set
-  connections.add(ws);
+// // Handle incoming WebSocket connections
+// wss.on("connection", (ws) => {
+//   // Add new connection to the set
+//   connections.add(ws);
 
-  // Remove closed connections from the set
-  ws.on("close", () => {
-    connections.delete(ws);
-  });
-});
+//   // Remove closed connections from the set
+//   ws.on("close", () => {
+//     connections.delete(ws);
+//   });
+// });
 
-// Route to receive camera feed from the producer
-app.post("/feed", (req, res) => {
-  let body = "";
+// // Route to receive camera feed from the producer
+// app.post("/feed", (req, res) => {
+//   let body = "";
 
-  req.on("data", (data) => {
-    // Accumulate the data chunks
-    body += data;
-  });
+//   req.on("data", (data) => {
+//     // Accumulate the data chunks
+//     body += data;
+//   });
 
-  req.on("end", () => {
-    // Handle the camera feed data sent by the producer
-    // and broadcast it to all connected consumers
+//   req.on("end", () => {
+//     // Handle the camera feed data sent by the producer
+//     // and broadcast it to all connected consumers
 
-    // Assuming the producer sends base64-encoded video data
-    connections.forEach((ws) => {
-      if (ws.readyState === WebSocket.OPEN) {
-        ws.send(body);
-      }
-    });
+//     // Assuming the producer sends base64-encoded video data
+//     connections.forEach((ws) => {
+//       if (ws.readyState === WebSocket.OPEN) {
+//         ws.send(body);
+//       }
+//     });
 
-    // Send a response to the producer
-    res.statusCode = 200;
-    res.end();
-  });
-});
+//     // Send a response to the producer
+//     res.statusCode = 200;
+//     res.end();
+//   });
+// });
 
 // Start the server
-server.listen(3000, () => {
+app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
---
+++ Jenkinsfile
@@ -0,0 +1,53 @@
+
+pipeline{
+
+	agent any
+
+	environment {
+		DOCKERHUB_CREDENTIALS=credentials('vss-docker-key')
+	}
+
+	stages {
+
+		stage('Build') {
+
+			steps {
+				sh 'docker build -t krishnap1999/video-streaming-platform:latest .'
+			}
+		}
+
+		stage('Login') {
+            steps {
+                withCredentials([usernamePassword(credentialsId: 'vss-docker-key', passwordVariable: 'DOCKERHUB_CREDENTIALS_PSW', usernameVariable: 'DOCKERHUB_CREDENTIALS_USR')]) {
+                    sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
+                }
+            }
+        }
+
+		stage('Push') {
+
+			steps {
+				sh 'docker push krishnap1999/video-streaming-platform:latest'
+			}
+		}
+		stage('Stop and Remove old Container') {
+            steps {
+                sh 'docker stop video-streaming-container || true'
+                sh 'docker rm video-streaming-container || true'
+            }
+        }
+        stage('Run Image') {
+
+			steps {
+				sh 'docker run -d -p 8081:8081 --name video-streaming-container krishnap1999/video-streaming-platform:latest'
+			}
+		}
+	}
+
+	post {
+		always {
+			sh 'docker logout'
+		}
+	}
+
+}
